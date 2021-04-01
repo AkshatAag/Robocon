0db0be302adc7bcd1c1b69c05d31a613ee182189
AkshatAag <81707536+AkshatAag@users.noreply.github.com>
2021-04-01 14:02:20 +0530

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,4 +1,4 @@
-
+import re
 def val(a):
     if a=='A':
         return 1
@@ -6,7 +6,22 @@ def val(a):
         return 2
     else :
         return 3
-
+def sum(s):
+    sum=0
+    for i in range(len(s)):
+        if s[i-1] in ['A','B','C']:
+            if i > len(s)-1 :
+                sum+=val(s[i-1])
+            elif s[i] in ['2','3','4','5','6','7','8','9']:
+                sum+=val(s[i-1]) * int(s[i])
+                i+=1
+            else:
+                sum+=val(s[i-1])
+    return sum
 str=input()
-x=str.split("(")
-print (ord(3))
+x=str.replace ( "(" , ')' )
+y=x.split(")")
+ans=0
+for i in range (len(y)):
+    ans+=sum(y[i-1])
+print(ans)
